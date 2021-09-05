185ef1497c63aa9c27a8e09072daf9b6b6d2055c
Gabriel B. G <zenoreude@gmail.com>
2021-09-05 17:36:46 -0300

    Add files via upload

--- tradutor_midi_risc-v.py
+++ tradutor_midi_risc-v.py
@@ -3,8 +3,8 @@ import os
 import platform as p
 FOLDER_PATH = dir_path = os.path.dirname(os.path.realpath(__file__))
 
-mid = mido.MidiFile('O seu arquivo MIDI')
-y = open("result.txt", "w")
+mid = mido.MidiFile('midi.mid')
+y = open("resultado.txt", "w")
 
 nlista = []
 plista = []
@@ -19,11 +19,15 @@ for i, track in enumerate(mid.tracks):
                 nlista.append(msg.note)
                 plista.append(int((mido.tick2second(msg.time, mid.ticks_per_beat,tempo)*1000)))
                 
-        elif msg.type == 'set_tempo':
+        if msg.type == 'set_tempo':
             tempo = msg.tempo
                 
 for i in range(len(nlista)):
-    y.write("," + str(nlista[i]) + "," + str(plista[i]))             
+    y.write("," + str(nlista[i]) + "," + str(plista[i]))
+    
+notas = len(nlista)
 
-     
+y.write("\n"*5)
+y.write("Número de notas:")
+y.write(str(notas))
 y.close()
