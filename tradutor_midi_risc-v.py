9bf5731cfc46b21cf80daa8c4e468e5eea9d9dec
Davi <48662592+d-magit@users.noreply.github.com>
2021-09-14 18:06:14 -0300

    Fix for multiple tracks
    Não tenho ctz se fiz isso certo. Mas ele tá salvando no seguinte modelo:
    """
    .data
    Track0: .word <tamanho>, <nota>, <tempo>, <nota>, <tempo>...
    
    Track1: .word <tamanho>, <nota>, <tempo>, <nota>, <tempo>...
    """
    
    Uso: "python tradutor_midi_risc-v.py <arquivo>.mid"

--- tradutor_midi_risc-v.py
+++ tradutor_midi_risc-v.py
@@ -1,33 +1,25 @@
 import mido
 import os
+import sys
 import platform as p
-FOLDER_PATH = dir_path = os.path.dirname(os.path.realpath(__file__))
+FOLDER_PATH = os.path.dirname(os.path.realpath(__file__))
 
-mid = mido.MidiFile('O seu arquivo midi vem aqui')
-y = open("resultado.txt", "w")
-
-nlista = []
-plista = []
-for i, track in enumerate(mid.tracks):
-    for msg in track:
-        if (not msg.is_meta):
-            if (msg.type == 'note_on'):
+mid = mido.MidiFile(FOLDER_PATH + sys.argv[1])
+with open("result.data", "w") as f:
+    for i, track in enumerate(mid.tracks):
+        nlista = []
+        plista = []
+        for msg in track:
+            if (not msg.is_meta and (msg.type == 'note_on' or msg.type == 'note_off')):
                 nlista.append(msg.note)
                 plista.append(int((mido.tick2second(msg.time, mid.ticks_per_beat,tempo)*1000)))
-                
-            elif (msg.type == 'note_off'):
-                nlista.append(msg.note)
-                plista.append(int((mido.tick2second(msg.time, mid.ticks_per_beat,tempo)*1000)))
-                
-        if msg.type == 'set_tempo':
-            tempo = msg.tempo
-                
-for i in range(len(nlista)):
-    y.write("," + str(nlista[i]) + "," + str(plista[i]))
-    
-notas = len(nlista)
+            if msg.type == 'set_tempo':
+                tempo = msg.tempo
+        
+        currenttrack = f"{len(nlista)}"
+        for j in range(len(nlista)):
+            currenttrack += ", " + str(nlista[j]) + ", " + str(plista[j])
+        currenttrack = f"Track{i}: .word {currenttrack} \n\n"
+        f.write(currenttrack)
 
-y.write("\n"*5)
-y.write("Número de notas:")
-y.write(str(notas))
-y.close()
+print("Done!")
